279063a91ff291e8a9b0f82cdca5148aedd749a0
Akihisa Higuchi <akihisa.nq@gmail.com>
2014-03-24 00:00:00 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BicycleTourManagerWeb::Application.config.secret_key_base = 'd1fad957f2cf0977928d07cdd40ec781eb58a1c1be933ce41943abaf8cac88fa97bb2747b7b304f3eec20c5264df1dc1ffa8c2b60581a30febcd7732a6dcd24b'
