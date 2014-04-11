76d8adc26446b9042e002df29a04035252d67d38
Akihisa Higuchi <akihisa.nq@gmail.com>
2014-04-12 00:00:00 +0900

    position カラムは持たないので date に変更

--- app/models/public_result_route.rb
+++ app/models/public_result_route.rb
@@ -2,5 +2,5 @@ class PublicResultRoute < ActiveRecord::Base
 	belongs_to :tour_result
 	acts_as_list scope: :tour_result
 
-	has_many :result_points, -> { order("position ASC") }
+	has_many :result_points, -> { order("time ASC") }
 end
