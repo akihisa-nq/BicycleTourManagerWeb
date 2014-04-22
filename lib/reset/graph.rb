145206b9639b7b115db0ed477ad20fdf40717e19
Akihisa Higuchi <akihisa.nq@gmail.com>
2014-04-23 00:00:00 +0900

    メモリを食いすぎるのを修正

--- lib/reset/graph.rb
+++ lib/reset/graph.rb
@@ -1,13 +1,13 @@
 class Reset::Graph
 	def self.regenerate_result
-		::TourResult.all.each do |tour|
+		::TourResult.find_each do |tour|
 			tour.need_update_graph = true
 			tour.save!
 		end
 	end
 
 	def self.regenerate_plan
-		::TourPlan.all.each do |plan|
+		::TourPlan.find_each do |plan|
 			::TourPlan.generate(plan.id)
 		end
 	end
