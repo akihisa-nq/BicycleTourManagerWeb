51628b600f420af458f7a9473463e2cb1b2dafbd
Akihisa Higuchi <akihisa.nq@gmail.com>
2016-08-31 00:00:00 +0900

    分割処理を修正

--- app/models/tour_plan_route.rb
+++ app/models/tour_plan_route.rb
@@ -9,16 +9,16 @@ class TourPlanRoute < ActiveRecord::Base
 		parser = RGeo::WKRep::WKBParser.new(private_line.factory, support_ewkb: true, support_wkb12: true)
 		connection = ActiveRecord::Base.connection
 
-		length = connection.select_one("SELECT ST_Length(Geography(private_line)) AS length FROM tour_plan_routes WHERE id = #{self.id}")["length"].to_f / 1000.0
+		length = (tour_plan_points.last.distance - tour_plan_points.first.distance).to_f
 		unit = 1.0 / length
 
 		tour_plan_points.each do |pt|
 			base = (pt.distance - tour_plan_points.first.distance).to_f / length
 
-			start = base - unit * 0.5
+			start = base - unit * 5.0
 			start = 0.0 if start < 0
 
-			finish = base + unit * 0.5
+			finish = base + unit * 5.0
 			finish = 1.0 if finish > 1.0
 
 			pt.near_line = parser.parse(connection.select_one("SELECT ST_LineSubstring(private_line, #{start}, #{finish}) AS line FROM tour_plan_routes WHERE id = #{self.id}")["line"])
