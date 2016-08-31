ceca2afa9d1029b3b42d93dc78d9e73e80544077
Akihisa Higuchi <akihisa.nq@gmail.com>
2016-09-01 00:00:00 +0900

    ちゃんと実装

--- app/controllers/api/exclusion_area_controller.rb
+++ app/controllers/api/exclusion_area_controller.rb
@@ -1,7 +1,8 @@
 module Api
 	class ExclusionAreaController < Api::BaseController
 		def list
-			render json: { name: "hoge" }
+			exclusion_areas = ExclusionArea.all_with_auth(current_user)
+			render json: { exclusion_areas: exclusion_areas }.to_json
 		end
 	end
 end
