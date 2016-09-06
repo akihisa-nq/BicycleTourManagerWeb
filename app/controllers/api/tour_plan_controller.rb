26566753f85619e5068702f16594a82affe14740
Akihisa Higuchi <akihisa.nq@gmail.com>
2016-09-07 00:00:00 +0900

    show api を追加

--- app/controllers/api/tour_plan_controller.rb
+++ app/controllers/api/tour_plan_controller.rb
@@ -1,12 +1,26 @@
 module Api
 	class TourPlanController < Api::BaseController
 		def list
-			tour_plans = TourPlan.all_with_auth(current_user, params[:offset], params[:limit]).map {|tour_plan| tour_plan.attributes }
-			tour_plans.each do |tour_plan|
-				tour_plan["published"] = tour_plan["published"] ? true : false
-				tour_plan.delete("planning_sheet_scale")
-			end
+			tour_plans = TourPlan
+				.all_with_auth(current_user, params[:offset], params[:limit])
+				.map {|tour_plan| tour_plan.attributes }
+			tour_plans.each { |tour_plan| filter_attributes_tour_plan(tour_plan) }
 			render json: { tour_plans: tour_plans }.to_json
+		end
+
+		def show
+			tour_plan = TourPlan
+				.find_with_auth(current_user, params[:id])
+			attrs = filter_attributes_tour_plan(tour_plan.attributes)
+			render json: attrs.to_json
+		end
+
+		private
+
+		def filter_attributes_tour_plan(tour_plan)
+			tour_plan["published"] = tour_plan["published"] ? true : false
+			tour_plan.delete("planning_sheet_scale")
+			tour_plan
 		end
 	end
 end
