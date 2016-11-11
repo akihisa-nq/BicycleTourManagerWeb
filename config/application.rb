1caa15814eda7bac4dee7fa5e6221ba01cfd4661
Akihisa Higuchi <akihisa.nq@gmail.com>
2016-11-11 21:14:03 +0900

    新規作成のアプリに合わせて色々更新

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
@@ -11,7 +11,7 @@ end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module BicycleTourManagerWeb
   class Application < Rails::Application
