dfe2c29d6e0541f12cb2bd997aea7bed75cff584
Akihisa Higuchi <akihisa.nq@gmail.com>
2016-11-15 20:37:22 +0900

    とりあえずエラー回避

--- db/migrate/20160823092938_change_elevation_to_z_axis.rb
+++ db/migrate/20160823092938_change_elevation_to_z_axis.rb
@@ -1,27 +1,73 @@
 class ChangeElevationToZAxis < ActiveRecord::Migration
 	def up
-		change_column :private_result_routes, :path, "GEOMETRY(LineStringZ,4326) USING ST_Force_3D(path)"
-		change_column :public_result_routes, :path, "GEOMETRY(LineStringZ,4326) USING ST_Force_3D(path)"
-		change_column :tour_plan_routes, :public_line, "GEOMETRY(LineStringZ,4326) USING ST_Force_3D(public_line)"
-		change_column :tour_plan_routes, :private_line, "GEOMETRY(LineStringZ,4326) USING ST_Force_3D(private_line)"
+		if PrivateResultRoutes.count > 0
+			change_column :private_result_routes, :path, "GEOMETRY(LineStringZ,4326) USING ST_Force_3D(path)"
+		else
+			change_column :private_result_routes, :path, "GEOMETRY(LineStringZ,4326)"
+		end
 
-		change_column :result_points, :point, "GEOMETRY(PointZ,4326) USING ST_Force_3D(point)"
+		if PublicResultRoutes.count > 0
+			change_column :public_result_routes, :path, "GEOMETRY(LineStringZ,4326) USING ST_Force_3D(path)"
+		else
+			change_column :public_result_routes, :path, "GEOMETRY(LineStringZ,4326)"
+		end
+
+		if TourPlanRoutes.count > 0
+			change_column :tour_plan_routes, :public_line, "GEOMETRY(LineStringZ,4326) USING ST_Force_3D(public_line)"
+			change_column :tour_plan_routes, :private_line, "GEOMETRY(LineStringZ,4326) USING ST_Force_3D(private_line)"
+		else
+			change_column :tour_plan_routes, :public_line, "GEOMETRY(LineStringZ,4326)"
+			change_column :tour_plan_routes, :private_line, "GEOMETRY(LineStringZ,4326)"
+		end
+
+		if ResultPoints.count > 0
+			change_column :result_points, :point, "GEOMETRY(PointZ,4326) USING ST_Force_3D(point)"
+		else
+			change_column :result_points, :point, "GEOMETRY(PointZ,4326)"
+		end
 		remove_column :result_points, :elevation
 
-		change_column :tour_plan_points, :point, "GEOMETRY(PointZ,4326) USING ST_Force_3D(point)"
+		if TourPlanPoints.count > 0
+			change_column :tour_plan_points, :point, "GEOMETRY(PointZ,4326) USING ST_Force_3D(point)"
+		else
+			change_column :tour_plan_points, :point, "GEOMETRY(PointZ,4326)"
+		end
 		remove_column :tour_plan_points, :elevation
 	end
 
 	def down
 		add_column :result_points, :elevation, :float
-		change_column :result_points, :point, "GEOMETRY(LineString,4326) USING ST_Force_2D(point)"
+		if ResultPoints.count > 0
+			change_column :result_points, :point, "GEOMETRY(LineString,4326) USING ST_Force_2D(point)"
+		else
+			change_column :result_points, :point, "GEOMETRY(LineString,4326)"
+		end
 
 		add_column :tour_plan_points, :elevation, :integer
-		change_column :tour_plan_points, :point, "GEOMETRY(LineString,4326) USING ST_Force_2D(point)"
+		if TourPlanPoints.count > 0
+			change_column :tour_plan_points, :point, "GEOMETRY(LineString,4326) USING ST_Force_2D(point)"
+		else
+			change_column :tour_plan_points, :point, "GEOMETRY(LineString,4326)"
+		end
 
-		change_column :private_result_routes, :path, "GEOMETRY(LineString,4326) USING ST_Force_2D(path)"
-		change_column :public_result_routes, :path, "GEOMETRY(LineString,4326) USING ST_Force_2D(path)"
-		change_column :tour_plan_routes, :public_line, "GEOMETRY(LineString,4326) USING ST_Force_2D(public_line)"
-		change_column :tour_plan_routes, :private_line, "GEOMETRY(LineString,4326) USING ST_Force_2D(private_line)"
+		if PrivateResultRoutes.count > 0
+			change_column :private_result_routes, :path, "GEOMETRY(LineString,4326) USING ST_Force_2D(path)"
+		else
+			change_column :private_result_routes, :path, "GEOMETRY(LineString,4326)"
+		end
+
+		if PublicResultRoutes.count > 0
+			change_column :public_result_routes, :path, "GEOMETRY(LineString,4326) USING ST_Force_2D(path)"
+		else
+			change_column :public_result_routes, :path, "GEOMETRY(LineString,4326)"
+		end
+
+		if TourPlanRoutes.count > 0
+			change_column :tour_plan_routes, :public_line, "GEOMETRY(LineString,4326) USING ST_Force_2D(public_line)"
+			change_column :tour_plan_routes, :private_line, "GEOMETRY(LineString,4326) USING ST_Force_2D(private_line)"
+		else
+			change_column :tour_plan_routes, :public_line, "GEOMETRY(LineString,4326)"
+			change_column :tour_plan_routes, :private_line, "GEOMETRY(LineString,4326)"
+		end
 	end
 end
