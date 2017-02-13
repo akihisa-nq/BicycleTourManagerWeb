92582692dde08e243ef5e070b108aab8fdc11eb8
Akihisa Higuchi <akihisa.nq@gmail.com>
2017-02-14 00:06:15 +0900

    例外をログ出力するように修正

--- app/jobs/generate_tile_job.rb
+++ app/jobs/generate_tile_job.rb
@@ -1,16 +1,21 @@
 class GenerateTileJob < ApplicationJob
-  queue_as :default
+	queue_as :default
 
-  def perform(*args)
-    kind = args[0].to_sym
-    id = args[1]
+	def perform(*args)
+		begin
+			kind = args[0].to_sym
+			id = args[1]
 
-	if kind == :tour_plan
-		plan = TourPlan.find(id)
-		plan.generate_tiles(true, 6, 14)
-	else
-		result = TourResult.find(id)
-		# FIXME
+			if kind == :tour_plan
+				plan = TourPlan.find(id)
+				plan.generate_tiles(true, 6, 14)
+			else
+				result = TourResult.find(id)
+				# FIXME
+			end
+		rescue => e
+			Rails.logger.fatal(e.message)
+			Rails.logger.fatal(e.backtrace.join("\n"))
+		end
 	end
-  end
 end
