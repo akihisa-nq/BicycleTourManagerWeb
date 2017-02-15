2a6c0df696aef461b3fcca1b810e556ccaba1d24
Akihisa Higuchi <akihisa.nq@gmail.com>
2017-02-15 11:17:31 +0900

    エラーになるので巻き戻し。元から Array ではないのだが。

--- app/models/exclusion_area.rb
+++ app/models/exclusion_area.rb
@@ -2,8 +2,6 @@ class ExclusionArea < ActiveRecord::Base
 	validates :point, presence: true
 	validates :distance, presence: true
 
-	attribute :point, :point
-
 	def self.all_with_auth(user)
 		if user.can?(:edit, ExclusionArea)
 			ExclusionArea.all
